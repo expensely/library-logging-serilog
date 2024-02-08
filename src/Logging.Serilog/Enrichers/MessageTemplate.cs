2d376345466c7676f6b14c9f023dd0341d9cf3af
Gavan Lamb <gavan.jon.lamb@gmail.com>
2024-02-09 00:58:00 +1100

    Add message template enricher

---
+++ src/Logging.Serilog/Enrichers/MessageTemplate.cs
@@ -0,0 +1,17 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Logging.Serilog.Enrichers;
+
+public class MessageTemplate : ILogEventEnricher
+{
+    public void Enrich(
+        LogEvent logEvent, 
+        ILogEventPropertyFactory propertyFactory)
+    {
+        var key = "MessageTemplate";
+        var value = logEvent.MessageTemplate.Text;
+        var property = propertyFactory.CreateProperty(key, value);
+        logEvent.AddOrUpdateProperty(property);
+    }
+}
