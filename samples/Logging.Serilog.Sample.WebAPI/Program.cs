cffd8254b41b285b3a80fc406eef5776405890f1
Gavan Lamb <gavan.jon.lamb@gmail.com>
2024-02-12 23:18:28 +1100

    Add web api sample project

---
+++ samples/Logging.Serilog.Sample.WebAPI/Program.cs
@@ -0,0 +1,34 @@
+using Logging.Serilog.Extensions;
+using Serilog;
+
+try
+{
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    builder.AddSerilog();
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
