ad88cde320c94363b98bbb0130ea1687e3b0b95f
Gavan Lamb <gavan.jon.lamb@gmail.com>
2024-04-16 22:35:00 +1000

    feature:[SCRUM-37] Add postcss to package.json and lock file

--- docs/public/js/main.js
+++ docs/public/js/main.js
@@ -7170,7 +7170,7 @@ limitations under the License.
           }
 
           var query = $(this).val();
-          var searchPage = 'http://localhost:1313/logging.serilog/search/?q=' + query;
+          var searchPage = 'http://localhost:1313/search/?q=' + query;
           document.location = searchPage;
 
           return false;
