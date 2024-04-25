f754c559140f3c47bb262fd67977982c4bba81f9
Gavan Lamb <gavan.jon.lamb@gmail.com>
2024-04-25 12:31:58 +1000

    feature: [SCRUM-37] Add documentation for RoutePattern enricher

--- src/Logging.Serilog/Enrichers/RoutePattern.cs
+++ src/Logging.Serilog/Enrichers/RoutePattern.cs
@@ -5,14 +5,32 @@ using Serilog.Events;
 
 namespace Logging.Serilog.Enrichers
 {
+    /// <summary>
+    /// Enricher for route pattern. This enricher will add the route pattern to the log event.
+    /// </summary>
     public class RoutePattern : ILogEventEnricher
     {
         private readonly IHttpContextAccessor _contextAccessor;
 
+        /// <summary>
+        /// Create route pattern enricher. Creates a new instance of HttpContextAccessor.
+        /// </summary>
         public RoutePattern() : this(new HttpContextAccessor()) { }
 
-        public RoutePattern(IHttpContextAccessor contextAccessor) => _contextAccessor = contextAccessor;
+        /// <summary>
+        /// Create route pattern enricher.
+        /// </summary>
+        /// <param name="contextAccessor">HTTP context accessor</param>
+        public RoutePattern(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
 
+        /// <summary>
+        /// Enrich log event with route pattern
+        /// </summary>
+        /// <param name="logEvent">Log event to enrich</param>
+        /// <param name="propertyFactory">Log event property factory</param>
         public void Enrich(
           LogEvent logEvent,
           ILogEventPropertyFactory propertyFactory)
@@ -24,15 +42,15 @@ namespace Logging.Serilog.Enrichers
             var routeEndpoint = _contextAccessor.HttpContext.GetEndpoint() as RouteEndpoint;
             var pattern = routeEndpoint?.RoutePattern.RawText;
 
-            if (routeData.Values.TryGetValue("version", out var version))
+            if (pattern?.Contains("{version:apiVersion}") == true)
             {
-                if (pattern?.Contains("{version:apiVersion}") == true)
+                if (routeData.Values.TryGetValue("version", out var version))
                 {
                     pattern = pattern.Replace("{version:apiVersion}", version.ToString());
                 }
             }
 
-            if (pattern?.StartsWith("/") == true)
+            if (pattern?.StartsWith("/") != true)
             {
                 pattern = "/" + pattern;
             }
