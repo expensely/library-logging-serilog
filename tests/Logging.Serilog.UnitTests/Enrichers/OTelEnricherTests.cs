6e4d9077197b31826a9bf17b407c17c22bee53a1
Gavan Lamb <gavan.jon.lamb@gmail.com>
2024-04-26 19:43:02 +1000

    feature: [SCRUM-37] Add unit tests for OTelEnricher

---
+++ tests/Logging.Serilog.UnitTests/Enrichers/OTelEnricherTests.cs
@@ -0,0 +1,47 @@
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using FluentAssertions;
+using Logging.Serilog.Enrichers;
+using Serilog.Core;
+using Serilog.Events;
+using Xunit;
+
+namespace Logging.Serilog.UnitTests.Enrichers;
+
+public class OTelEnricherTests
+{
+    private readonly IFixture _fixture =
+        new Fixture().Customize(new AutoNSubstituteCustomization { ConfigureMembers = true });
+    
+    [Fact]
+    public void Enrich_NullLogEvent_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var enricher = new OTelEnricher();
+        LogEvent logEvent = null;
+        ILogEventPropertyFactory propertyFactory = _fixture.Create<ILogEventPropertyFactory>();
+
+        // Act
+        Action act = () => enricher.Enrich(logEvent, propertyFactory);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("logEvent (Parameter 'logEvent cannot be null')");
+    }
+    
+    [Fact]
+    public void Enrich_NullPropertyFactory_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var enricher = new OTelEnricher();
+        LogEvent logEvent = _fixture.Create<LogEvent>();
+        ILogEventPropertyFactory propertyFactory = null;
+
+        // Act
+        Action act = () => enricher.Enrich(logEvent, propertyFactory);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("propertyFactory (Parameter 'propertyFactory cannot be null')");
+    }
+}
